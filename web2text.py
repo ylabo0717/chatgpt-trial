f0ce3425357ae3111037635e7a0d1da4814ce17a
Yosuke Horie <Yosuke.Horie@sony.com>
2023-05-05 18:16:51 +0900

    delete comment

--- web2text.py
+++ web2text.py
@@ -2,23 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 def crawl_site(url, depth=1):
-    # 深さが0になったら終了
     if depth <= 0:
         return
 
-    # URLからページのHTMLデータを取得
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # HTMLからテキストを抽出
     text = soup.get_text()
 
-    # ファイルに保存
     filename = url.split("/")[-1]
     with open(f"./data/{filename}.txt", "w") as f:
         f.write(text)
 
-    # リンクを抽出して再帰的に処理
     for link in soup.find_all("a"):
         if "href" in link.attrs:
             next_url = link.attrs["href"]
